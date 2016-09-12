38c98fa0c926548ed18bdce8bc676a4fd3df2a1e
Justin Ledoux <justin.ledoux@justinweb.pro>
2016-09-12 10:31:06 -0400

    config

--- config.js
+++ config.js
@@ -4,50 +4,18 @@ module.exports = {
 	local: {
 		baseURL: 								'http://localhost:' + process.env.PORT + '/',
 		staticResourcesBaseURL: '/',
-		jQuery: 								'/js-dev/lib/jquery-3.0.0.min.js',
 		staticResourceCache: 		{ maxage: 0, etag: false },
-		mongodb: {
-			options: {
-				server: {
-					socketOptions: { keepAlive: 1 }
-				}
-			},
-			connectionString: 'mongodb://localhost:27017/justinweb-local'
-		},
-		s3: {
-			accessKeyId: 'Your access key',
-			secretAccessKey: 'Your secret access key',
-			region: 'us-west-2'
-		}
 	},
 
 	staging: {
-		baseURL: 								'http://justinweb.heroku.com/',
+		baseURL: 								'http://mainautzmann.heroku.com/',
 		staticResourcesBaseURL: '/',
-		jQuery: 								'/js-dev/lib/jquery-3.0.0.min.js',
 		staticResourceCache: 		{ maxage: 0, etag: false },
-		mongodb: {
-			options: {
-				server: {
-					socketOptions: { keepAlive: 1 }
-				}
-			},
-			connectionString: process.env.MONGOLAB_URI
-		}
 	},
 
 	production: {
-		baseURL: 								'http://www.justinweb.pro/',
+		baseURL: 								'http://mainautzmann.heroku.com/',
 		staticResourcesBaseURL: '/',
-		jQuery: 								'/js-dev/lib/jquery-3.0.0.min.js',
 		staticResourceCache: 		{ maxage: 0, etag: false },
-		mongodb: {
-			options: {
-				server: {
-					socketOptions: { keepAlive: 1 }
-				}
-			},
-			connectionString: process.env.MONGOLAB_URI
-		}
 	}
 }
